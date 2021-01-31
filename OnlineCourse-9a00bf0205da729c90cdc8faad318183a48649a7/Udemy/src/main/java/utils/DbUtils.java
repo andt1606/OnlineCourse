3fcd9f19a4db734ea4ecc57abb33b18aca6f18d4
Thanhhoang112 <18110112@student.hcmute.edu.vn>
2021-01-31 18:01:18 +0700

    Sửa lỗi log out
    Sửa lỗi logout

--- OnlineCourse-9a00bf0205da729c90cdc8faad318183a48649a7/Udemy/src/main/java/utils/DbUtils.java
+++ OnlineCourse-9a00bf0205da729c90cdc8faad318183a48649a7/Udemy/src/main/java/utils/DbUtils.java
@@ -5,7 +5,7 @@ import org.sql2o.Sql2o;
 
 public class DbUtils {
 
-    static final Sql2o sql2o = new Sql2o("jdbc:mysql://localhost:3306/udemy", "root", "");
+    static final Sql2o sql2o = new Sql2o("jdbc:mysql://localhost:3306/udemy", "root", "0616259001");
 
     public static Connection getConnection() {
         return sql2o.open();
